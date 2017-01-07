66718592e144d28ffcf7093066e6e150f8fc2de6
Dwight Hubbard <d@dhub.me>
2017-01-06 23:20:35 -0800

    Update metadata

--- setup.py
+++ setup.py
@@ -297,7 +297,7 @@ if __name__ == '__main__':
         )
         del args['ext_modules']
         del args['cmdclass']
-        add_redis_metadata(WINDOWS_REDIS_SERVER, METADATA_FILENAME)
+        add_redis_metadata(server_filename=WINDOWS_REDIS_SERVER, metadata_filename=METADATA_FILENAME)
     else:
         if sys.platform in UNSUPPORTED_PLATFORMS:
             print(
